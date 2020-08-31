d50605aa84c07a632d1c0e19e0e3bc5590da9747
Byron-Edwards <byron_edwards@outlook.com>
2020-08-31 13:27:23 +0800

    wrap up the policy

--- Policy_New.py
+++ Policy_New.py
@@ -8,18 +8,15 @@ class Policy:
         self.game = game
         self.player_num = player_num
         self.action_map = self.game.action_map
-        self.me = self.game.agent if player_num else self.game.opponent
-        self.opp = self.game.opponent if player_num else self.game.agent
+        self.offset_action = dict()
+        for k, v in self.action_map.items():
+            self.offset_action[tuple(v)] = k
         self.my_goal = self.game.getGoalLocation(player_num)
         self.opp_goal = self.game.getGoalLocation(not player_num)
-        self.has_ball = self.game.agent_keep_ball if player_num else not self.game.agent_keep_ball
-        self.policy_type = [1,2,3,4]
-        # policy 1: if not has ball, get ball. if has ball get to goal
-        # policy 3: if not has ball, get ball. if has ball get to goal and keep away
-        # policy 2: if not has ball, defence the goal if distance larger than 2, if less than 2 get closer. if has ball get to goal
-        # policy 4: if not has ball, defence the goal if distance larger than 2. if less than 2 get closer. if has ball get to goal and keep away
+        self.update_status()
+        self.policy_type = [self.policy0, self.policy1, self.policy2, self.policy3]
 
-    def moving(self, closer: bool,player_number:bool, locations: np.ndarray, actions: np.ndarray):
+    def moving(self, closer: bool, player_number: bool, locations: np.ndarray, actions: np.ndarray, weight=(1, 1, 1)):
         assert locations.ndim == 2 and actions.ndim == 2
         current_location = self.me if player_number else self.opp
         current_dis = np.sum(abs(locations - current_location), axis=1)
@@ -34,7 +31,7 @@ class Policy:
         min_dis = np.array(min_dis) - np.min(current_dis)
         max_dis = np.array(max_dis) - np.max(current_dis)
         mean_dis = np.array(mean_dis) - np.mean(current_dis)
-        scores = (min_dis + max_dis + mean_dis) * (1 if not closer else -1)
+        scores = (weight[0] * min_dis + weight[1] * max_dis + weight[2] * mean_dis) * (1 if not closer else -1)
         scores[np.argwhere(scores < 0)] = 0
         return scores
 
@@ -47,64 +44,136 @@ class Policy:
         new_locations = current_location + all_actions
         valid_actions = self.game.validLocationAll(new_locations)
         valid_new_locations = new_locations[valid_actions]
-        valid_actions_index = np.arange(len(new_locations))[valid_actions]
-        return valid_new_locations, valid_actions_index
+        all_actions = all_actions[valid_actions]
+        return valid_new_locations, all_actions
 
-    def grab_ball(self):
-        assert not self.has_ball
-        assert sum(abs(self.me - self.opp)) <= 1
+    def update_status(self):
+        self.has_ball = self.game.agent_keep_ball if self.player_num else not self.game.agent_keep_ball
+        self.me = self.game.agent if self.player_num else self.game.opponent
+        self.opp = self.game.opponent if self.player_num else self.game.agent
+
+    def grab_ball(self, valid_actions):
+        actions_score = self.moving(closer=True, player_number=False, locations=self.opp_goal, actions=valid_actions)
+        action = self.score_to_index(valid_actions, actions_score)
+        opp_new_location = self.opp + self.action_map[action]
+        offset = opp_new_location - self.me
+        if tuple(offset) in self.offset_action:
+            return self.offset_action[tuple(offset)]
+        else:
+            return None
+
 
     def keep_ball(self):
         assert self.has_ball
         assert sum(abs(self.me - self.opp)) <= 1
 
+    def win_the_game(self, valid_actions):
+        current_location = self.me
+        goal = self.my_goal
+        new_locations = current_location + valid_actions
+        for location, action in zip(new_locations,valid_actions):
+            if (location == goal).all(1).any():
+                return action
+        return None
+
+    def score_to_index(self, valid_actions, actions_score):
+        action_index = []
+        for offset in valid_actions:
+            action_index.append(self.offset_action[tuple(offset)])
+        if sum(actions_score) == 0:
+            actions_prob = [1/len(actions_score) for i in range(len(actions_score))]
+        else:
+            actions_prob = actions_score / sum(actions_score)
+        action = np.random.choice(action_index, p=actions_prob)
+        return action
+
+    # policy 1: if not has ball, get ball. if has ball get to goal
+    def policy0(self, valid_actions):
+        if self.has_ball:
+            offset = self.win_the_game(valid_actions)
+            if offset is not None:
+                return self.offset_action[tuple(offset)]
+            actions_score = self.moving(closer=True, player_number=True, locations=self.my_goal, actions=valid_actions)
+        else:
+            actions_score = self.moving(closer=True, player_number=True, locations=np.expand_dims(self.opp, axis=0), actions=valid_actions)
+            if self.game.getPlayerDistance() <= 2:
+                action = self.grab_ball(valid_actions)
+                if action is not None:
+                    return action
+        action = self.score_to_index(valid_actions, actions_score)
+        return action
+
+    # policy 3: if not has ball, get ball. if has ball get to goal and keep away from opponent
     def policy1(self, valid_actions):
         if self.has_ball:
-            actions_score = self.moving(closer=True, player_number=True, locations=self.my_goal,actions=valid_actions)
+            offset = self.win_the_game(valid_actions)
+            if offset is not None:
+                return self.offset_action[tuple(offset)]
+            actions_score = self.moving(closer=True, player_number=True, locations=self.my_goal, actions=valid_actions, weight=(2, 2, 2))
+            actions_score += self.moving(closer=False, player_number=True, locations=np.expand_dims(self.opp, axis=0), actions=valid_actions, weight=(1, 1, 1))
         else:
-            actions_score = self.moving(closer=True, player_number=True, locations=self.opp, actions=valid_actions)
-        actions_prob = actions_score / sum(actions_score)
-        return np.random.choice(valid_actions, p=actions_prob)
+            actions_score = self.moving(closer=True, player_number=True, locations=np.expand_dims(self.opp, axis=0), actions=valid_actions)
+            if self.game.getPlayerDistance() <= 2:
+                action = self.grab_ball(valid_actions)
+                if action is not None:
+                    return action
+        action = self.score_to_index(valid_actions, actions_score)
+        return action
 
+    # policy 2:
+    # if not has ball, defence the goal if distance larger than 2. if less than 2 get closer try to grab the ball.
+    # if has ball get to goal
     def policy2(self, valid_actions):
         if self.has_ball:
+            offset = self.win_the_game(valid_actions)
+            if offset is not None:
+                return self.offset_action[tuple(offset)]
             actions_score = self.moving(closer=True, player_number=True, locations=self.my_goal, actions=valid_actions)
-            actions_score += self.moving(closer=False, player_number=True, locations=self.opp, actions=valid_actions)
+            actions_score += self.moving(closer=False, player_number=True, locations=np.expand_dims(self.opp, axis=0), actions=valid_actions)
         else:
-            actions_score = self.moving(closer=True, player_number=True, locations=self.opp, actions=valid_actions)
-        actions_prob = actions_score / sum(actions_score)
-        return np.random.choice(valid_actions, p=actions_prob)
+            actions_score = self.moving(closer=True, player_number=True, locations=self.opp_goal, actions=valid_actions)
+            if self.game.getPlayerDistance() <= 2:
+                action = self.grab_ball(valid_actions)
+                if action is not None:
+                    return action
+        action = self.score_to_index(valid_actions, actions_score)
+        return action
 
+    # policy 3:
+    # if not has ball, defence the goal if distance larger than 2. if less than 2 get closer try to grab the ball.
+    # if has ball get to goal and keep away from opponent
     def policy3(self, valid_actions):
         if self.has_ball:
-            actions_score = self.moving(closer=True, player_number=True, locations=self.my_goal, actions=valid_actions)
-            actions_score += self.moving(closer=False, player_number=True, locations=self.opp, actions=valid_actions)
-        else:
-            actions_score = self.moving(closer=True, player_number=True, locations=self.opp_goal, actions=valid_actions)
-        actions_prob = actions_score / sum(actions_score)
-        return np.random.choice(valid_actions, p=actions_prob)
-
-    def policy4(self, valid_actions):
-        if self.has_ball:
+            offset = self.win_the_game(valid_actions)
+            if offset is not None:
+                return self.offset_action[tuple(offset)]
             actions_score = self.moving(closer=True, player_number=True, locations=self.my_goal, actions=valid_actions)
         else:
             actions_score = self.moving(closer=True, player_number=True, locations=self.opp_goal, actions=valid_actions)
-        actions_prob = actions_score / sum(actions_score)
-        return np.random.choice(valid_actions, p=actions_prob)
+            if self.game.getPlayerDistance() <= 2:
+                action = self.grab_ball(valid_actions)
+                if action is not None:
+                    return action
+        action = self.score_to_index(valid_actions, actions_score)
+        return action
 
     def get_actions(self, policy_num):
-        valid_new_locations, valid_actions_index = self.validActionAll(player_number=self.player_num)
+        self.update_status()
+        valid_new_locations, all_actions = self.validActionAll(player_number=self.player_num)
+        return self.policy_type[policy_num](all_actions)
 
 
 if __name__ == "__main__":
     policy_types = list(range(5))
-    env = Soccer()
+    env = SoccerPLUS()
     env.reset()
     my_policy = Policy(game=env, player_num=True)
+    opp_policy = Policy(game=env, player_num=False)
     while True:
+        s_, reward, done, _ = env.step(my_policy.get_actions(1), opp_policy.get_actions(1))
         env.render()
-        s_, reward, done, _ = env.step(my_policy.get_actions(1), np.random.randint(env.n_actions))
         if done:
             env.reset()
-        time.sleep(1)
+            env.render()
+        time.sleep(0.1)
 
